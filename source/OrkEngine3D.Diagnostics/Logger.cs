10ff0f78979a9d74a32e9dec31a9efd1b71e9fea
jwoff78 <57271436+jwoff78@users.noreply.github.com>
2021-07-10 23:29:11 -0500

    Started to implement Diagnostics System.
    At a loss of what to do, I am starting at Diagnostics System, Different than Stride Engine they have it under core, but we will want to log everything sophisticatedly so user is never lost in the dust. So I have a dedicated a cs project to it to ensure we have grips on whole system.

---
+++ source/OrkEngine3D.Diagnostics/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrkEngine3D.Diagnostics
+{
+    public class Logger : ILogger
+    {
+        public void Log(ILogMessege logMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
